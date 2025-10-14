f3a8472d3da046483e45139c89d7b9517e5f6639
Bugracanu <bugracanusta@gmail.com>
2025-10-14 20:46:12 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using basics.Models;
+using AspNetCoreGeneratedDocument;
+
+namespace basics.Controllers;
+
+public class HomeController : Controller {
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+}
